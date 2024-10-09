bdda4bc71cc60377159a29fdccb58e84274ee22d
“Renáta <koselak.renata87@gmail.com>
2024-10-09 09:34:33 +0200

    projekt

--- hazi03/index.ts
+++ hazi03/index.ts
@@ -4,3 +4,4 @@ const myTodoList = new TodoList();
 myTodoList.addItem({ id: 1, content: "Vásárlás", dueDate: new Date('2024.10.04')});
 myTodoList.addItem({ id: 2, content: "Munka", dueDate: new Date("2024.10.07") });
 myTodoList.addItem({ id: 3, content: "Tanulás", dueDate: new Date("2024.10.05") });
+
